2fb223ecf7981fb91464769ee7476725798ff29f
Marc Enzo <emarc237@gmail.com>
2025-03-18 03:43:37 +0100

    feat: Add backup settings to Settings model

--- macos_app/lib/models/settings.dart
+++ macos_app/lib/models/settings.dart
@@ -7,12 +7,18 @@ class Settings {
   final bool startMinimized;
   final bool autoImportFromClipboard;
 
+  // Backup settings
+  final bool autoBackupEnabled;
+  final int maxBackups;
+
   Settings({
     this.themeMode = ThemeMode.system,
     this.dataFolderPath = '',
     this.showSystemTrayIcon = true,
     this.startMinimized = false,
     this.autoImportFromClipboard = false,
+    this.autoBackupEnabled = true,
+    this.maxBackups = 10,
   });
 
   Settings copyWith({
@@ -21,6 +27,8 @@ class Settings {
     bool? showSystemTrayIcon,
     bool? startMinimized,
     bool? autoImportFromClipboard,
+    bool? autoBackupEnabled,
+    int? maxBackups,
   }) {
     return Settings(
       themeMode: themeMode ?? this.themeMode,
@@ -28,6 +36,8 @@ class Settings {
       showSystemTrayIcon: showSystemTrayIcon ?? this.showSystemTrayIcon,
       startMinimized: startMinimized ?? this.startMinimized,
       autoImportFromClipboard: autoImportFromClipboard ?? this.autoImportFromClipboard,
+      autoBackupEnabled: autoBackupEnabled ?? this.autoBackupEnabled,
+      maxBackups: maxBackups ?? this.maxBackups,
     );
   }
 
@@ -38,42 +48,37 @@ class Settings {
       'showSystemTrayIcon': showSystemTrayIcon,
       'startMinimized': startMinimized,
       'autoImportFromClipboard': autoImportFromClipboard,
+      'autoBackupEnabled': autoBackupEnabled,
+      'maxBackups': maxBackups,
     };
   }
 
   factory Settings.fromJson(Map<String, dynamic> json) {
     return Settings(
       themeMode: ThemeMode.values[json['themeMode'] as int],
-      dataFolderPath: json['dataFolderPath'] as String,
-      showSystemTrayIcon: json['showSystemTrayIcon'] as bool,
-      startMinimized: json['startMinimized'] as bool,
-      autoImportFromClipboard: json['autoImportFromClipboard'] as bool,
+      dataFolderPath: json['dataFolderPath'] as String? ?? '',
+      showSystemTrayIcon: json['showSystemTrayIcon'] as bool? ?? true,
+      startMinimized: json['startMinimized'] as bool? ?? false,
+      autoImportFromClipboard: json['autoImportFromClipboard'] as bool? ?? false,
+      autoBackupEnabled: json['autoBackupEnabled'] as bool? ?? true,
+      maxBackups: json['maxBackups'] as int? ?? 10,
     );
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-  
-    return other is Settings &&
-      other.themeMode == themeMode &&
-      other.dataFolderPath == dataFolderPath &&
-      other.showSystemTrayIcon == showSystemTrayIcon &&
-      other.startMinimized == startMinimized &&
-      other.autoImportFromClipboard == autoImportFromClipboard;
+
+    return other is Settings && other.themeMode == themeMode && other.dataFolderPath == dataFolderPath && other.showSystemTrayIcon == showSystemTrayIcon && other.startMinimized == startMinimized && other.autoImportFromClipboard == autoImportFromClipboard && other.autoBackupEnabled == autoBackupEnabled && other.maxBackups == maxBackups;
   }
 
   @override
   int get hashCode {
-    return themeMode.hashCode ^
-      dataFolderPath.hashCode ^
-      showSystemTrayIcon.hashCode ^
-      startMinimized.hashCode ^
-      autoImportFromClipboard.hashCode;
+    return themeMode.hashCode ^ dataFolderPath.hashCode ^ showSystemTrayIcon.hashCode ^ startMinimized.hashCode ^ autoImportFromClipboard.hashCode ^ autoBackupEnabled.hashCode ^ maxBackups.hashCode;
   }
 
   @override
   String toString() {
-    return 'Settings(themeMode: $themeMode, dataFolderPath: $dataFolderPath, showSystemTrayIcon: $showSystemTrayIcon, startMinimized: $startMinimized, autoImportFromClipboard: $autoImportFromClipboard)';
+    return 'Settings(themeMode: $themeMode, dataFolderPath: $dataFolderPath, showSystemTrayIcon: $showSystemTrayIcon, startMinimized: $startMinimized, autoImportFromClipboard: $autoImportFromClipboard, autoBackupEnabled: $autoBackupEnabled, maxBackups: $maxBackups)';
   }
 }
